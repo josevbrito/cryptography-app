c6f0e1a5e20c24d29968b86f2b92ad4ed9b8ee9a
josevbrito <josevictorbritoc@gmail.com>
2024-03-23 18:32:17 -0300

    Settings Update

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,3 +1,4 @@
+import 'package:cryptocode/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:cryptocode/layouts/appbar.dart';
 
@@ -14,16 +15,58 @@ class _SettingsPageState extends State<SettingsPage> {
     return Scaffold(
       appBar: const CustomAppBar(title: 'Settings'),
       body: Center(
-          child: ListView(
-            children: [
-              ListTile(
-                leading: const Icon(Icons.color_lens),
-                title: const Text('Theme'),
-                trailing: const Icon(Icons.keyboard_arrow_right),
-                onTap: () {
-                },
+          child: Padding(
+            padding: const EdgeInsets.only(top: 50, bottom: 50, left: 25, right: 25),
+            child: Container(
+              decoration: BoxDecoration(
+                color: theme.primaryColor
               ),
-            ],
+              child: ListView(
+                children: [
+
+                  // Theme
+                  ListTile(
+                    leading: const Icon(Icons.color_lens),
+                    title: const Text('Theme'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {
+                      // route
+                    },
+                  ),
+                  
+                  // Language
+                  ListTile(
+                    leading: const Icon(Icons.translate),
+                    title: const Text('Language'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {
+                      // route
+                    },
+                  ),
+
+                  // Charts
+                  ListTile(
+                    leading: const Icon(Icons.add_chart_outlined),
+                    title: const Text('Charts'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {
+                      // route
+                    },
+                  ),
+
+                  // Other works
+                  ListTile(
+                    leading: const Icon(Icons.code),
+                    title: const Text('Other codes'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {
+                      // route
+                    },
+                  ),
+
+                ],
+              ),
+            ),
           ),
         )
       );
